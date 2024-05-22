40dced5356f2f2974da1371d4fbe0c8b63e92c32
Rokas Reizgys <rokas.student@gmail.com>
2024-05-22 13:34:45 +0300

    rollback callback url

--- controllers/front/redirect.php
+++ controllers/front/redirect.php
@@ -39,8 +39,7 @@ class SpectrocoinRedirectModuleFrontController extends ModuleFrontController {
 			'BTC', // pay currency code
 			$total, // receive amount
 			$currency->iso_code, // receive currency code
-			"https://cms-prestashop.bankera.cloud/module/spectrocoin/callback",// debug 
-			// $this->context->link->getModuleLink('spectrocoin', 'callback'), // callback url 	
+			$this->context->link->getModuleLink('spectrocoin', 'callback'), // callback url 	// debug 
 			$this->context->link->getModuleLink('spectrocoin', 'validation'), // success url
 			$this->context->link->getModuleLink('spectrocoin', 'cancel'), // failure url
 			$this->module->lang, // lang
