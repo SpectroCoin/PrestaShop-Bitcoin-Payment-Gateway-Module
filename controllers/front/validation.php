afca84bb5d5764a9f8ad3c26fff8c07d4171d18c
Rokas Reizgys <rokas.student@gmail.com>
2024-08-20 11:06:47 +0300

    strict typing to validation.php

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -12,17 +12,17 @@ class SpectrocoinValidationModuleFrontController extends ModuleFrontController {
     /**
      * @see FrontController::postProcess()
      */
-    public function postProcess() {
+    public function postProcess(): void {
         $cart = $this->context->cart;
-        if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active) {
+        if ($cart->id_customer === 0 || $cart->id_address_delivery === 0 || $cart->id_address_invoice === 0 || !$this->module->active) {
             Tools::redirect('index.php?controller=order&step=1');
         }
 
-        $customer = new Customer($cart->id_customer);
+        $customer = new Customer((int)$cart->id_customer);
         if (!Validate::isLoadedObject($customer)) {
             Tools::redirect('index.php?controller=order&step=1');
         }
 
-        Tools::redirect('index.php?controller=order-confirmation&id_cart=' . $cart->id . '&id_module=' . $this->module->id . '&id_order=' . $this->module->currentOrder . '&key=' . $customer->secure_key);
+        Tools::redirect('index.php?controller=order-confirmation&id_cart=' . (int)$cart->id . '&id_module=' . (int)$this->module->id . '&id_order=' . (int)$this->module->currentOrder . '&key=' . $customer->secure_key);
     }
 }
