1e3a801c5f919e64d082261103bb18b9da23a435
SteinCode <rokas.student@gmail.com>
2025-02-13 09:02:34 +0200

    2.0.0

--- SCMerchantClient/Config.php
+++ SCMerchantClient/Config.php
@@ -13,6 +13,7 @@ class Config
     const MERCHANT_API_URL = 'https://test.spectrocoin.com/api/public';
     const AUTH_URL = 'https://test.spectrocoin.com/api/public/oauth/token';
     const ACCEPTED_FIAT_CURRENCIES = ["EUR", "USD", "PLN", "CHF", "SEK", "GBP", "AUD", "CAD", "CZK", "DKK", "NOK"];
+    const PUBLIC_SPECTROCOIN_CERT_LOCATION = "https://test.spectrocoin.com/public.pem"; // https://spectrocoin.com/files/merchant.public.pem'
 
     const SPECTROCOIN_ACCESS_TOKEN_CONFIG_KEY = 'SPECTROCOIN_ACCESS_TOKEN';
 }
