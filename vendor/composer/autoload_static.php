f439348722b32c66e898592dca85a6ef70fd6425
SpectroCoin <noreply@spectrocoin.com>
2026-07-24 18:10:07 +0200

    2.1.1: update guzzlehttp/guzzle to 7.15.1 and guzzlehttp/psr7 to 2.13.0
    Resolves Dependabot security advisories for guzzlehttp/guzzle and
    guzzlehttp/psr7 (composer.lock + vendored sources). Dependency-only change.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,83 +4,89 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit52f4d09da5448f317e649c18691e0f69
+class ComposerStaticInit1e28aff234b72f53a19a656082cf26ef
 {
     public static $files = array (
-        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
     public static $prefixLengthsPsr4 = array (
-        'S' => 
+        'S' =>
         array (
+            'Symfony\\Polyfill\\Php80\\' => 23,
             'SpectroCoin\\SCMerchantClient\\' => 29,
-            'SpectroCoin\\Controllers\\Front\\' => 30,
         ),
-        'P' => 
+        'P' =>
         array (
             'Psr\\Http\\Message\\' => 17,
             'Psr\\Http\\Client\\' => 16,
         ),
-        'G' => 
+        'G' =>
         array (
             'GuzzleHttp\\Psr7\\' => 16,
             'GuzzleHttp\\Promise\\' => 19,
             'GuzzleHttp\\' => 11,
         ),
-        'C' => 
+        'C' =>
         array (
             'Composer\\Installers\\' => 20,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
-        'SpectroCoin\\SCMerchantClient\\' => 
+        'Symfony\\Polyfill\\Php80\\' =>
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
+        ),
+        'SpectroCoin\\SCMerchantClient\\' =>
         array (
             0 => __DIR__ . '/../..' . '/SCMerchantClient',
         ),
-        'SpectroCoin\\Controllers\\Front\\' => 
-        array (
-            0 => __DIR__ . '/../..' . '/controllers/front',
-        ),
-        'Psr\\Http\\Message\\' => 
+        'Psr\\Http\\Message\\' =>
         array (
             0 => __DIR__ . '/..' . '/psr/http-factory/src',
             1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
-        'Psr\\Http\\Client\\' => 
+        'Psr\\Http\\Client\\' =>
         array (
             0 => __DIR__ . '/..' . '/psr/http-client/src',
         ),
-        'GuzzleHttp\\Psr7\\' => 
+        'GuzzleHttp\\Psr7\\' =>
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
         ),
-        'GuzzleHttp\\Promise\\' => 
+        'GuzzleHttp\\Promise\\' =>
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
         ),
-        'GuzzleHttp\\' => 
+        'GuzzleHttp\\' =>
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
-        'Composer\\Installers\\' => 
+        'Composer\\Installers\\' =>
         array (
             0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
         ),
     );
 
     public static $classMap = array (
+        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit52f4d09da5448f317e649c18691e0f69::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit52f4d09da5448f317e649c18691e0f69::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit52f4d09da5448f317e649c18691e0f69::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit1e28aff234b72f53a19a656082cf26ef::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit1e28aff234b72f53a19a656082cf26ef::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit1e28aff234b72f53a19a656082cf26ef::$classMap;
 
         }, null, ClassLoader::class);
     }
